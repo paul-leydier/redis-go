9a0e7f31184e2b178683641c83c38ab4e80f9143
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-02 00:08:17 +0100

    ✅ unit test multiple commands

--- src/server_test.go
+++ src/server_test.go
@@ -16,6 +16,7 @@ func Test_Listen(t *testing.T) {
 }
 
 func Test_Ping(t *testing.T) {
+	// A "PING" command should receive a "PONG" response
 	server, client := net.Pipe()
 	go func() {
 		serve(server)
@@ -33,5 +34,29 @@ func Test_Ping(t *testing.T) {
 	}
 	if string(resp) != "+PONG\r\n" {
 		t.Fatalf("invalid response: expected %s, got %s", "+PONG", resp)
+	}
+}
+
+func Test_Multiple_Pings(t *testing.T) {
+	// A single connection should be able to send multiple commands
+	server, client := net.Pipe()
+	go func() {
+		serve(server)
+	}()
+	msg := []byte("PING")
+	for i := 0; i < 10; i++ {
+		_, err := client.Write(msg)
+		if err != nil {
+			t.Fatalf("could not write to localhost:6379 - %s", err)
+		}
+		resp := make([]byte, 64)
+		_, err = client.Read(resp)
+		resp = bytes.Trim(resp, "\x00")
+		if err != nil {
+			t.Fatalf("could not read from localhost:6379 - %s", err)
+		}
+		if string(resp) != "+PONG\r\n" {
+			t.Fatalf("invalid response: expected %s, got %s", "+PONG", resp)
+		}
 	}
 }
