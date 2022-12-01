30897e0dc9f72c3da08702e11e3ba03505ced522
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-02 00:00:46 +0100

    ✅ test ping

--- src/server_test.go
+++ src/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
@@ -11,5 +12,26 @@ func Test_Listen(t *testing.T) {
 	_, err := net.Dial("tcp", "localhost:6379")
 	if err != nil {
 		t.Fatalf("could not connect to localhost:6379 - %s", err)
+	}
+}
+
+func Test_Ping(t *testing.T) {
+	server, client := net.Pipe()
+	go func() {
+		serve(server)
+	}()
+	msg := []byte("PING")
+	_, err := client.Write(msg)
+	if err != nil {
+		t.Fatalf("could not write to localhost:6379 - %s", err)
+	}
+	resp := make([]byte, 64)
+	_, err = client.Read(resp)
+	resp = bytes.Trim(resp, "\x00")
+	if err != nil {
+		t.Fatalf("could not read from localhost:6379 - %s", err)
+	}
+	if string(resp) != "+PONG\r\n" {
+		t.Fatalf("invalid response: expected %s, got %s", "+PONG", resp)
 	}
 }
