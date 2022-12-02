57149f714384fb0df10eb81a083bb2e364c72a56
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-02 22:38:21 +0100

    ✅ unit test RESP encoding functions

---
+++ src/core/core_test.go
@@ -0,0 +1,20 @@
+package core
+
+import "testing"
+
+func TestRespEncodeSimpleString(t *testing.T) {
+	// Encoding a simple string format
+	encoded := RespEncode(SimpleString, "lorem ipsum")
+	const expected = "+lorem ipsum\r\n"
+	if string(encoded) != expected {
+		t.Fatalf("excpected %s, got %b", expected, encoded)
+	}
+}
+
+func TestRespDecodeSimpleString(t *testing.T) {
+	decoded := RespDecode([]byte("+lorem ipsum\r\n"))
+	const expected = "lorem ipsum"
+	if decoded != expected {
+		t.Fatalf("expected %s, got %s", expected, decoded)
+	}
+}
