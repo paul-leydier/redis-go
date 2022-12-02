366d1eba23b161c03520c7f0f248d5113b9c14da
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-03 00:02:23 +0100

    ✅ unit test Ping side texts

--- src/client/client_test.go
+++ src/client/client_test.go
@@ -29,3 +29,15 @@ func TestClient_Ping(t *testing.T) {
 		t.Fatalf("expected 'PONG' response, got %s", response)
 	}
 }
+
+func TestClient_PingWithText(t *testing.T) {
+	serverConn, client := MockServerClient()
+	go server.Serve(serverConn)
+	response, err := client.Ping("toto")
+	if err != nil {
+		t.Fatalf("error during call to Client.Ping - %s", err)
+	}
+	if response != "toto" {
+		t.Fatalf("expected 'toto' response, got %s", response)
+	}
+}
