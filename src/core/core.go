ae9d955f50accf53e90e6ec5a3273e160fd7b323
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-03 01:00:25 +0100

    ⚡️ fast parseBulkString

--- src/core/core.go
+++ src/core/core.go
@@ -45,13 +45,27 @@ func RespDecode(msg []byte) (RespType, string) {
 		decoded := string(msg[1:])
 		return Error, strings.TrimRight(decoded, "\r\n")
 	case '$':
-		parts := bytes.SplitN(msg, []byte("\r\n"), 2)
-		if len(parts) <= 2 {
-			log.Panicf("cannot decode BulkString %b", msg)
-		}
-		return BulkString, string(bytes.TrimRight(parts[1], "\r\n"))
+		return BulkString, parseBulkString(msg)
 	default:
 		log.Panicf("unknown msg type identifier %b", msg[0])
 	}
 	return Error, "should not be here"
+}
+
+func parseBulkStringNaive(msg []byte) string {
+	parts := bytes.SplitN(msg, []byte("\r\n"), 2)
+	if len(parts) < 2 {
+		log.Panicf("cannot decode BulkString %b", msg)
+	}
+	return string(bytes.TrimRight(parts[1], "\r\n"))
+}
+
+func parseBulkString(msg []byte) string {
+	msgLength := uint8(0)
+	prefixLength := 0
+	for i := 1; msg[i] != '\r'; i++ {
+		msgLength = (msgLength * 10) + (msg[i] - '0')
+		prefixLength = i
+	}
+	return string(msg[prefixLength+2 : prefixLength+2+int(msgLength)])
 }
