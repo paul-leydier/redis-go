5638396f1816868a2378511438702d1deeca5a68
Paul Leydier <paul.leydier@compass-ft.com>
2022-12-03 01:10:36 +0100

    ✅ unit test BulkString decoder

--- src/core/core_test.go
+++ src/core/core_test.go
@@ -41,6 +41,40 @@ func TestRespDecodeSimpleString(t *testing.T) {
 	}
 }
 
+func TestRespDecodeBulkString(t *testing.T) {
+	// Decoding BulkString format
+	respType, decoded := RespDecode([]byte("$5\r\nhello\r\n"))
+	if respType != BulkString {
+		t.Fatalf("invalid response type: expected %d, got %d", BulkString, respType)
+	}
+	const expected = "hello"
+	if decoded != expected {
+		t.Fatalf("expected %s, got %s", expected, decoded)
+	}
+}
+
+func TestRespDecodeBulkStringEmpty(t *testing.T) {
+	respType, decoded := RespDecode([]byte("$0\r\n\r\n"))
+	if respType != BulkString {
+		t.Fatalf("invalid response type: expected %d, got %d", BulkString, respType)
+	}
+	const expected = ""
+	if decoded != expected {
+		t.Fatalf("expected %s, got %s", expected, decoded)
+	}
+}
+
+func TestRespDecodeBulkStringWithReturns(t *testing.T) {
+	respType, decoded := RespDecode([]byte("$14\r\nhello \r\nworld!\r\n"))
+	if respType != BulkString {
+		t.Fatalf("invalid response type: expected %d, got %d", BulkString, respType)
+	}
+	const expected = "hello \r\nworld!"
+	if decoded != expected {
+		t.Fatalf("expected %s, got %s", expected, decoded)
+	}
+}
+
 func BenchmarkParseBulkStringNaive(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		parseBulkStringNaive([]byte("$14\r\nhello \r\nworld!\r\n"))
